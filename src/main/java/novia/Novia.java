32ec8efe0c9faa92d8a0d81b33b8d6973d450b10
valdogarnica <valdogarnica151@gmail.com>
2024-09-30 01:06:07 -0600

    subiendo quieres ser mi novia a git

---
+++ src/main/java/novia/Novia.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package novia;
+
+/**
+ *
+ * @author root
+ */
+public class Novia {
+
+    public static void main(String[] args) {
+        new esposa().setVisible(true);
+    }
+}
